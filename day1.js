e6e412da2b929c5d1746fc59588c79950c5ef3fa
Bastiaan van Graafeiland <bastiaan89@live.nl>
2016-12-01 20:53:14 +0100

    clean up after day 1

--- day1.js
+++ day1.js
@@ -1,7 +1,6 @@
 'use strict';
 
 let input = 'L2 L5 L5 R5 L2 L4 R1 R1 L4 R2 R1 L1 L4 R1 L4 L4 R5 R3 R1 L1 R1 L5 L1 R5 L4 R2 L5 L3 L3 R3 L3 R4 R4 L2 L5 R1 R2 L2 L1 R3 R4 L193 R3 L5 R45 L1 R4 R79 L5 L5 R5 R1 L4 R3 R3 L4 R185 L5 L3 L1 R5 L2 R1 R3 R2 L3 L4 L2 R2 L3 L2 L2 L3 L5 R3 R4 L5 R1 R2 L2 R4 R3 L4 L3 L1 R3 R2 R1 R1 L3 R4 L5 R2 R1 R3 L3 L2 L2 R2 R1 R2 R3 L3 L3 R4 L4 R4 R4 R4 L3 L1 L2 R5 R2 R2 R2 L4 L3 L4 R4 L5 L4 R2 L4 L4 R4 R1 R5 L2 L4 L5 L3 L2 L4 L4 R3 L3 L4 R1 L2 R3 L2 R1 R2 R5 L4 L2 L1 L3 R2 R3 L2 L1 L5 L2 L1 R4';
-// let input = 'R8 R4 R4 R8';
 
 let movements = input.split(' ');
 
@@ -14,8 +13,6 @@ for (var i = 0; i < movements.length; i++) {
         break;
     }
 }
-
-// console.log(Math.abs(x) + Math.abs(y));
 
 function turnAndMove(movement) {
     let direction = movement[0];
@@ -31,10 +28,10 @@ function turnAndMove(movement) {
     currentDirectionAngle = (currentDirectionAngle + difference + 360) % 360;
 
     for (var i = 0; i < steps; i++) {
-        x += Math.round(Math.sin(currentDirectionAngle * (Math.PI / 180)));
-        y += Math.round(Math.cos(currentDirectionAngle * (Math.PI / 180)));
+        x += getDelta(Math.sin);
+        y += getDelta(Math.cos);
         if (visitedLocations.some(coords => coords.x == x && coords.y == y)) {
-            console.log(Math.abs(x) + Math.abs(y));
+            console.log(totalDistance());
             return true;
         }
         else {
@@ -43,5 +40,13 @@ function turnAndMove(movement) {
     }
 
     return false;
+}
+
+function totalDistance() {
+    return Math.abs(x) + Math.abs(y);
+}
+
+function getDelta(func) {
+    return Math.round(func(currentDirectionAngle * (Math.PI / 180)));
 }
 
