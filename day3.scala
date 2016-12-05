0f942a6da2a8efd507a89549d20b2400227b0f6f
Bastiaan van Graafeiland <bastiaan89@live.nl>
2016-12-05 15:08:21 +0100

    day 3 part 2

--- day3.scala
+++ day3.scala
@@ -1,14 +1,19 @@
 object day3 {
   def main(args: Array[String]) {
-    val lines = input.split("\n").map(input => input.trim)
-    val numValid = lines.map(lineToTriangle).count(validTriangle)
-    println(numValid)
+    val sides = input.split("\n").flatMap(splitLine).toList
+    val columns = getColumn(sides, 0) ++ getColumn(sides, 1) ++ getColumn(sides, 2)
+    println(numValidTriangles(columns))
   }
 
-  def lineToTriangle(line: String) = {
-    val sides = line.split("""\s+""").map(Integer.parseInt)
-    (sides(0), sides(1), sides(2))
-  }
+  def numValidTriangles(columns: List[Int]): Int =
+    columns match {
+      case List() => 0
+      case _ => (if (validTriangle((columns.head, columns(1), columns(2)))) 1 else 0) + numValidTriangles(columns.drop(3))
+    }
+
+  def getColumn(sides: List[Int], i: Int) = sides.zipWithIndex.filter(_._2 % 3 == i).map(_._1)
+
+  def splitLine(line: String) = line.trim.split("""\s+""").map(Integer.parseInt)
 
   def validTriangle(triangle: (Int, Int, Int)) = triangle._1 + triangle._2 > triangle._3 && triangle._1 + triangle._3 > triangle._2 && triangle._2 + triangle._3 > triangle._1
 
