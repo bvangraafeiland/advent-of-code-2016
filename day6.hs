ea742577688245c26b4888776ee3470edbdc307c
Bastiaan van Graafeiland <bastiaan89@live.nl>
2016-12-06 15:18:34 +0100

    day 6 part 2

--- day6.hs
+++ day6.hs
@@ -1,16 +1,12 @@
 module Main where
-import qualified Data.Map as Map
 import Data.List
 
 getCode :: [String] -> String
-getCode messageLines = map mostOccurringCharacter $ linesToColumns messageLines
-
-linesToColumns :: [String] -> [String]
-linesToColumns = transpose
+getCode messageLines = map mostOccurringCharacter $ transpose messageLines
 
 mostOccurringCharacter :: String -> Char
 mostOccurringCharacter string = character
-    where (character, _) = maximumBy (\(_, length1) (_, length2) -> compare length1 length2) $ map (\x -> (head x, length x)) $ group $ sort string
+    where (character, _) = minimumBy (\(_, length1) (_, length2) -> compare length1 length2) $ map (\x -> (head x, length x)) $ group $ sort string
 
 main = do
     fileContent <- readFile "./input/day6.txt"
